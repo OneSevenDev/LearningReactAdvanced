9327786c0d93f83462d50d79c272458fee6207e3
manuelguarniz <cruzemg95@gmail.com>
2019-01-21 21:38:37 -0500

    Send parameters

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-const Location = () => (
-  <div><h1>Perú</h1></div>
-);
+const Location = ({ city }) => {
+  //const { city } = props;
+  return (
+    <div><h1>{city}</h1></div>
+  );
+}
 
 export default Location;
--- src/components/WeatherLocation.js
+++ src/components/WeatherLocation.js
@@ -4,7 +4,7 @@ import WeatherData from './WeatherData'
 
 const WeatherLocation = () => (
   <div>
-    <Location />
+    <Location city={"Perú"} />
     <WeatherData />
   </div>
 );

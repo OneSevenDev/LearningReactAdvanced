34629be9506084edfb93c287541037f7ef4a9dae
manuelguarniz <cruzemg95@gmail.com>
2019-01-28 22:45:54 -0500

    concat api weather

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,6 +4,12 @@ import WeatherData from './WeatherData'
 import { SUN, WINDY } from './../../constants/weathers'
 import './styles.css'
 
+const location = "Buenos Aires, ar";
+const api_key = "7f5525e739d5c5d34c8d0be8320af62d";
+const url_base_weather = "https://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
 const data = {
   temperature: 20,
   weatherState: SUN,
@@ -27,6 +33,7 @@ class WeatherLocation extends Component {
     }
   }
   handleUpdateClick = () => {
+    fetch(api_weather);
     this.setState({
       city: "Huanchaco",
       data: dataTest,
